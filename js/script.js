3b1e5b02a77402dd135d91cc57c631f2334cc909
Marcin Trawiński <trawinski1@wp.pl>
2018-02-04 18:59:26 +0100

    Tidy up nav-popup js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,40 @@
 // document.addEventListener("DOMContentLoaded", function(event) {
   const navPopup = document.querySelector('.nav-popup');
-  const navButton = document.querySelector('.nav-popup__foreground');
+  const navButton = navPopup.querySelector('.nav-popup__foreground');
   const navBackground = document.querySelector('.nav-popup__background');
   const navContent = document.querySelector('.nav-popup__container');
-  const navListItems = document.querySelectorAll('.nav-popup__item');
+  const navListItems = navContent.querySelectorAll('.nav-popup__item');
   const topNavBar = document.querySelector('.nav-container');
 
-  function showButton(e) {
-    // console.log(e);
+  function showButton() {
     if (window.scrollY > topNavBar.offsetHeight) {
       navPopup.classList.add('nav-popup-visible');
-    } 
-    else {
+    } else {
       if (navPopup.classList.contains('nav-popup-active') && navButton.classList.contains('nav-popup__foreground-active') && window.scrollY < topNavBar.offsetHeight) {
-        console.log("aaaa");
         return;
-      }
-
-      else {
+      } else {
         navPopup.classList.remove('nav-popup-visible');
-
       }
     }
-  }
+  };
 
   function openNav() {
-    // navButton.classList.toggle('nav-popup__foreground-active');
-    // if (navButton.classList.contains('nav-popup__foreground-active')) {
-      navPopup.classList.add('nav-popup-active');
-      navBackground.classList.add('nav-popup__background-active')
-      navContent.classList.add('nav-popup__container-active');
+    navPopup.classList.add('nav-popup-active');
+    navBackground.classList.add('nav-popup__background-active')
+    navContent.classList.add('nav-popup__container-active');
 
-      navListItems.forEach(item => item.addEventListener('click', closeNav))
-    // }
+    navListItems.forEach(item => item.addEventListener('click', closeNav));
   };
 
   function closeNav() {
-    
     navBackground.classList.remove('nav-popup__background-active');
     navContent.classList.remove('nav-popup__container-active');
     navButton.classList.remove('nav-popup__foreground-active');
-    
+    if (window.scrollY < topNavBar.offsetHeight) {
+      setTimeout(() => {
+        navPopup.classList.remove('nav-popup-visible');
+      }, 800);
+    }
   };
 
   function handleNav(e) {
@@ -49,39 +43,12 @@
       openNav();
     } else {
       closeNav();
-      if (window.scrollY < topNavBar.offsetHeight) {
-        setTimeout(() => {
-          navPopup.classList.remove('nav-popup-visible');
-          console.log("bbb");
-        }, 800);
-      }
     }
-  }
+  };
 
   window.addEventListener('scroll', showButton);
   navButton.addEventListener('click', handleNav);
 
-  /* navButton.addEventListener('click', function(e) {
-    navButton.classList.toggle('nav-popup__foreground-active');
-    if (navButton.classList.contains('nav-popup__foreground-active')) {
-      navPopup.classList.add('nav-popup-active');
-      navBackground.classList.add('nav-popup__background-active')
-      navContent.classList.add('nav-popup__container-active');
-
-      navListItems.forEach(item => item.addEventListener('click', function(e){
-        navBackground.classList.remove('nav-popup__background-active');
-        navContent.classList.remove('nav-popup__container-active');
-        navButton.classList.remove('nav-popup__foreground-active');
-      }))
-    } else {
-      navPopup.classList.remove('nav-popup-active');
-      navBackground.classList.remove('nav-popup__background-active');
-      navContent.classList.remove('nav-popup__container-active');
-    }
-    // console.log(Array.from([navButton.classList.value, navPopup.classList.value, navBackground.classList.value, navContent.classList.value]));
-  }) */
-// });
-  
   /* Map settings */
   function initMap(){
     const options = {
